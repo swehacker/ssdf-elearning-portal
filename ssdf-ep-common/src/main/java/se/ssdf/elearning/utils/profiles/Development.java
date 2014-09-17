eeaaefba03596d7cc30426cc1e84d230992f1547
Patrik Falk <patrik@patrikfalk.com>
2014-09-17 21:49:38 +0200

    Changed name on the profiles

--- ssdf-ep-common/src/main/java/se/ssdf/elearning/utils/profiles/Development.java
+++ ssdf-ep-common/src/main/java/se/ssdf/elearning/utils/profiles/Development.java
@@ -10,5 +10,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Profile("dev")
-public @interface Dev {
+public @interface Development {
 }
--- ssdf-ep-common/src/main/java/se/ssdf/elearning/utils/profiles/Production.java
+++ ssdf-ep-common/src/main/java/se/ssdf/elearning/utils/profiles/Production.java
@@ -10,5 +10,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Profile("prod")
-public @interface Prod {
+public @interface Production {
 }
